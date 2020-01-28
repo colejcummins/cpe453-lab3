82920d947f5f25206b59c3adb253208794d0f8e9
Cole Cummins <ccummi08@unix2.csc.calpoly.edu>
2020-01-28 11:50:15 -0800

    broken code sick

--- sorting_threads.c
+++ sorting_threads.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <sys/stat.h>
+
+int *unsorted;
+
+int comparator(const void *int1, const void *int2);
+void single_thread(int *unsorted, int length);
 
 int main(int argc, char *argv[]) {
     FILE *in;
     char temp[40];
-    int *unsorted, i;
+    int i;
     struct stat st;
 
     if (argc != 2) {
@@ -29,9 +35,34 @@ int main(int argc, char *argv[]) {
 
     while (fgets(temp, 40, in) != NULL) {
         unsorted[i] = atoi(temp);
+        printf("%d\n", unsorted[i]);
         i++;
     }
 
+    single_thread(unsorted, i - 1);
     free(unsorted);
     return 0;
+}
+
+void single_thread(int *unsorted, int length) {
+    int i;
+    int *unsorted_copy = (int *)malloc(length * sizeof(int));
+    struct timespec start, finish;
+    double elapsed;
+    memcpy(unsorted_copy, unsorted, length * sizeof(int));
+
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    qsort(unsorted_copy, length * sizeof(int), sizeof(int), comparator);
+    clock_gettime(CLOCK_MONOTONIC, &finish);
+
+    elapsed = (finish.tv_sec - start.tv_sec);
+    elapsed += (finish.tv_nsec - start.tv_nsec) / 1000000000.0;
+    printf("Single threaded sorting takes %f sec\n", elapsed);
+    for (i = 0; i < length; i++);
+        printf("%d\n", unsorted_copy[i]);
+    free(unsorted_copy);
+}
+
+int comparator(const void *int1, const void *int2) {
+    return (int *)int1 - (int *)int2;
 }
