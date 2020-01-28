429556ac5baab929fe367816c220b9049226da63
Cole Cummins <ccummi08@unix2.csc.calpoly.edu>
2020-01-28 10:30:19 -0800

    read file

---
+++ sorting_threads.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+
+int main(int argc, char *argv[]) {
+    FILE *in;
+    char *temp;
+    int *unsorted, i;
+    struct stat st;
+
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s filename\n", argv[0]);
+        exit(EXIT_FAILURE);
+    }
+
+    in = fopen(argv[1], "r");
+
+    // Print error and exit if file cannot be opened
+    if (in == NULL) {
+        fprintf(stderr, "%s: ", argv[1]);
+        perror("");
+        exit(EXIT_FAILURE);
+    }
+
+    stat(argv[1], &st);
+    int *unsorted = (int *)malloc(st.st_size);
+    i = 0;
+    while (fgets(temp, 40, in) != NULL) {
+        unsorted[i] = stoi(temp);
+        i++;
+    }
+
+    return 0;
+}
