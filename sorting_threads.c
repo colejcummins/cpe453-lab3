cee90e2d0191f0c01d509245bd4ee3fc1c863fce
Cole Bemis <colebemis@gmail.com>
2020-01-29 19:44:53 -0800

    Run merge thread

--- sorting_threads.c
+++ sorting_threads.c
@@ -3,18 +3,22 @@
 #include <string.h>
 #include <time.h>
 #include <sys/stat.h>
+#include <pthread.h>
 
 int *unsorted;
 int *sorted;
 
 int comparator(const void *int1, const void *int2);
 void single_thread(int *unsorted, int length);
+void *merge();
 
 int main(int argc, char *argv[]) {
     FILE *in;
     char temp[40];
     int i;
     struct stat st;
+    pthread_t tid = 2;
+    pthread_attr_t attr;
 
     if (argc != 2) {
         fprintf(stderr, "Usage: %s filename\n", argv[0]);
@@ -41,6 +45,13 @@ int main(int argc, char *argv[]) {
     }
 
     single_thread(unsorted, i);
+
+    // Set up merge thread
+    pthread_attr_init(&attr);
+    pthread_create(&tid, &attr, merge, NULL);
+    pthread_join(tid, NULL);
+    printf("done\n");
+
     //free(unsorted);
     //free(sorted);
     return 0;
@@ -64,4 +75,10 @@ void single_thread(int *unsorted, int length) {
 
 int comparator(const void *int1, const void *int2) {
     return *(int *)int1 - *(int *)int2;
+}
+
+// Will be run in a seperate thread to merge two sorted lists
+void *merge() {
+    printf("Merging!!!\n");
+    pthread_exit(0);
 }
